f330c6e04f30634bdacad3b98459323456bb7a36
Dakseyyy <Dakseyxd@proton.me>
2026-02-28 10:20:53 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -412,11 +412,15 @@ const ComplexApp = () => (
 const getIsIOS = (): boolean => {
   try {
     const v = document.createElement('video');
+    const gl = document.createElement('canvas').getContext('webgl');
+    const renderer = gl?.getParameter(gl.RENDERER) ?? '';
+    
     const checks = {
       GestureEvent: 'GestureEvent' in window,
       webkitPresentation: 'webkitSupportsPresentationMode' in v,
       webkitConvert: 'webkitConvertPointFromNodeToPage' in window,
       WebKitCSSMatrix: 'WebKitCSSMatrix' in window,
+      appleGPU: renderer.includes('Apple'),
       touch: ('ontouchstart' in window || navigator.maxTouchPoints > 0)
     };
 
